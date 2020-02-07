71a2fe2818be59389dec8527417900642ce437ec
sanaeram5 <sanaeram5@gmail.com>
2020-02-08 00:56:07 +0530

    claculator app(state1)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Calculator from './components/calculator.js';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Render the Calculator to the Web page.
+ReactDOM.render(<Calculator />, document.getElementById('root'));
